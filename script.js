85df883afa8d5c184cb8865bdebd89749357a51d
amberyeap <amberyeap02@gmail.com>
2026-04-07 00:29:35 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/* sample of APA style reference
+journal
+Chorpita, B. F., & Barlow, D. H. (2018). The Development of Anxiety: 
+The Role of Control in the Early Environment. Routledge EBooks, 227–264.
+https://doi.org/10.4324/9781315619996-2
+
+Miller, K., & Massie, M. J. (2006). Depression and Anxiety. 
+The Cancer Journal, 12(5), 388–397. 
+https://journals.lww.com/journalppo/Abstract/2006/09000/Depression_and_Anxiety.8.aspx
+In-text citation: (Miller & Massie, 2006)
+
+sample of UKM STYLE citation
+Andanastuti Muchtar, Lim, L.C. & Lee., K.H. 2002. Finite element analysis of
+Vickers indentation cracking processes in brittle solids using elements
+exhibiting cohesive post failure behaviour. Journal of Materials Science 38(2):
+235-243.
+
+Hamzah, N., Mohamed, A. & Hussain, A. 2004. A new approach to locate the voltage
+sag source using real current component. Journal of Electric Power System
+Research 72: 113-123.
+
+Author’s name, followed by a full stop
+Year, followed by a full stop
+Title of article, followed by a full stop
+Title of Journal must be italicized (First letter of each word in the title, except for
+prepositions, should be capitalized)
+Publication data (volume, series number, month or season)
+Issue number (within brackets), followed by a colon - can skip for now
+Number of pages of the article, followed by a full stop
+*/
+
